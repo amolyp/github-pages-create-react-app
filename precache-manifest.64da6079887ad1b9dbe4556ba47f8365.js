6ee17d71b5e91b0fba30121ceee16ea5a0b18eb3
Amol Palshetkar <amol.palshetkar@gmail.com>
2020-03-06 23:02:59 +0530

    Updates

---
+++ precache-manifest.64da6079887ad1b9dbe4556ba47f8365.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf68dd627f4fdee710b1f84c74ead8d",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "51d9ed9d410df02eb3a6",
+    "url": "/github-pages-create-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a1c72baacb35bdbedaec",
+    "url": "/github-pages-create-react-app/static/js/2.5eaada2d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github-pages-create-react-app/static/js/2.5eaada2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d9ed9d410df02eb3a6",
+    "url": "/github-pages-create-react-app/static/js/main.ffe28736.chunk.js"
+  },
+  {
+    "revision": "f3d1193c854dd36e6557",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.a87b8d72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
